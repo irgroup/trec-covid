f41c404a1d2bfc872b506cafa9cdd1e3e9a46046
Philipp Schaer <philipp.schaer@googlemail.com>
2020-04-16 22:29:08 +0200

    fixed formating

--- getDataSets.sh
+++ getDataSets.sh
@@ -10,13 +10,13 @@ cd $DOWNLOAD_PATH
 urls=(
     https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/comm_use_subset.tar.gz
     https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/noncomm_use_subset.tar.gz
-	https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/custom_license.tar.gz
-	https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/biorxiv_medrxiv.tar.gz
-	https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/metadata.csv
-	https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/antiviral_with_properties.sdf.gz
-	https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/cord19_specter_embeddings_2020-04-10.tar.gz
+    https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/custom_license.tar.gz
+    https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/biorxiv_medrxiv.tar.gz
+    https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/metadata.csv
+    https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/antiviral_with_properties.sdf.gz
+    https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/2020-04-10/cord19_specter_embeddings_2020-04-10.tar.gz
 )
-	
+
 for i in "${urls[@]}"; do
     wget -c "$i"
 done
