5ffa9a75e0b104b701c7af75f0b1a52bd8f4eb38
breuert <timo.breuer@th-koeln.de>
2020-04-19 14:16:13 +0200

    add rerank draft

---
+++ rerank.py
@@ -0,0 +1,20 @@
+
+
+if __name__ == '__main__':
+    # get baseline ranking from run
+
+    # get document data by doc_id
+
+    # make d_pack out of it
+
+    # load model
+
+    # predict score for each doc
+
+    # dict with score docid mapping
+
+    # sort by descending score (rerank)
+
+    # write output
+
+    pass
