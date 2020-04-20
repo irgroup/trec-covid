f648fd7175305c509a48f0451f718dab3ab5d53b
breuert <timo.breuer@th-koeln.de>
2020-04-20 17:02:15 +0200

    add option to fetch fulltexts from pmc

--- config.py
+++ config.py
@@ -11,17 +11,21 @@ DATA = './data'
 META = './download/metadata.csv'
 VALID_ID = './download/docids-rnd1.txt'
 ESEARCH = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&sort=relevance&term='
+ESEARCH_PMC = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pmc&sort=relevance&term='
 EFETCH = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id='
+EFETCH_PMC = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc&id='
 RETMODE = '&retmode=xml'
 PUBMED_FETCH = './artifact/pubmed/'
-PUBMED_DUMP_DATE = '2020-04-19'
+PUBMED_DUMP_DATE = '2020-04-20'
 MODEL_DUMP = './artifact/model/'
 MODEL_TYPE = 'drmm'  # 'dense', 'drmm'
 RUN_DIR = './artifact/runs/'
 RERANKED_RUN = 'reranked_drmm'
 
-PUBMED_SCRAPE = True
+PUBMED_SCRAPE = False
 PUBMED_FRONT = 'https://pubmed.ncbi.nlm.nih.gov/?term='
 RESULT_SIZE = 20
+
+FULLTEXT_PMC = False
 
 RERANK_WEIGHT = 0.5
--- fetchPubmedData.py
+++ fetchPubmedData.py
@@ -4,7 +4,9 @@ import requests as rq
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-from config import EFETCH, ESEARCH, RETMODE, PUBMED_FETCH, PUBMED_SCRAPE, PUBMED_FRONT, RESULT_SIZE, TOPIC
+from config import (EFETCH, ESEARCH, RETMODE, PUBMED_FETCH, PUBMED_SCRAPE,
+                    PUBMED_FRONT, RESULT_SIZE, TOPIC, ESEARCH_PMC, EFETCH_PMC,
+                    FULLTEXT_PMC)
 from util import query_dict
 
 queries = query_dict(TOPIC)
@@ -25,13 +27,20 @@ for k, v in queries.items():
         for id_field in soup.find_all('span', {"class": "docsum-pmid"}):
             ids = ids + id_field.text + ','
     else:
-        res = rq.get(ESEARCH + v)
+        if FULLTEXT_PMC:
+            res = rq.get(ESEARCH_PMC + v)
+        else:
+            res = rq.get(ESEARCH + v)
+
         soup = BeautifulSoup(res.content, 'lxml')
         ids = ''
         for id_field in soup.idlist.find_all('id'):
             ids = ids + id_field.text + ','
 
-    res = rq.get(EFETCH + ids + RETMODE)
+    if FULLTEXT_PMC:
+        res = rq.get(EFETCH_PMC + ids + RETMODE)
+    else:
+        res = rq.get(EFETCH + ids + RETMODE)
 
     soup = BeautifulSoup(res.content, 'lxml')
 
