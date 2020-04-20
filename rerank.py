6840b8ffe4d94dcc9848e540bc7b65f0fee80e3c
breuert <timo.breuer@th-koeln.de>
2020-04-20 14:32:35 +0200

    adapt rerank for drmm

--- rerank.py
+++ rerank.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import matchzoo as mz
 from config import META, MODEL_DUMP, MODEL_TYPE, RERANKED_RUN, DATA, RUN_DIR, BASELINE, topic
 from util import query_dict, map_sha_path, test_data, train_data
@@ -16,6 +17,8 @@ if __name__ == '__main__':
                               names=['topic', 'Q0', 'cord_uid', 'rank', 'score', 'team'],
                               index_col=False)
 
+    glove_embedding = mz.datasets.embeddings.load_glove_embedding(dimension=300)
+
     with open(os.path.join(RUN_DIR, RERANKED_RUN), 'w') as run:
         for topic_number, query in query_dict(topic).items():
             topic_df = df_baseline[df_baseline['topic'] == int(topic_number)]
@@ -24,17 +27,41 @@ if __name__ == '__main__':
             # make datapack
             d_pack_test = test_data(topic_number, cord_uids, query, meta, msp)
 
-            # prepare preprocessor
-            train_raw = train_data(topic_number)
-            preprocessor = mz.preprocessors.BasicPreprocessor()
-            preprocessor.fit(train_raw)
+            if MODEL_TYPE == 'dense':
+                # load model
+                model = mz.load_model(os.path.join(MODEL_DUMP, MODEL_TYPE, str(topic_number)))
 
-            # transform document data
-            test_processed = preprocessor.transform(d_pack_test)
-            test_x, test_y = test_processed.unpack()
+                # prepare preprocessor
+                train_raw = train_data(topic_number)
+                preprocessor = mz.preprocessors.BasicPreprocessor()
+                preprocessor.fit(train_raw)
 
-            # load model
-            model = mz.load_model(os.path.join(MODEL_DUMP, MODEL_TYPE, str(topic_number)))
+                # transform document data
+                test_processed = preprocessor.transform(d_pack_test)
+                test_x, test_y = test_processed.unpack()
+
+            if MODEL_TYPE == 'drmm':
+                # load model
+                model = mz.load_model(os.path.join(MODEL_DUMP, MODEL_TYPE, str(topic_number)))
+                task = mz.tasks.Ranking()
+                train_raw = train_data(topic_number)
+                preprocessor = mz.preprocessors.BasicPreprocessor(fixed_length_left=10,
+                                                                  fixed_length_right=100,
+                                                                  remove_stop_words=False)
+                preprocessor.fit(train_raw)
+
+                test_processed = preprocessor.transform(d_pack_test)
+                embedding_matrix = glove_embedding.build_matrix(preprocessor.context['vocab_unit'].state['term_index'])
+                # normalize the word embedding for fast histogram generating.
+                l2_norm = np.sqrt((embedding_matrix * embedding_matrix).sum(axis=1))
+                embedding_matrix = embedding_matrix / l2_norm[:, np.newaxis]
+                model.load_embedding_matrix(embedding_matrix)
+                hist_callback = mz.data_generator.callbacks.Histogram(embedding_matrix,
+                                                                      bin_size=30,
+                                                                      hist_mode='LCH')
+                test_generator = mz.DataGenerator(data_pack=test_processed, mode='point',
+                                                  callbacks=[hist_callback])
+                test_x, test_y = test_generator[:]
 
             # predict score for each doc
             scores = model.predict(test_x)
