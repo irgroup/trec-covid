ad4088a1fe82b817f4573166f27fb2cd4f04549f
breuert <timo.breuer@th-koeln.de>
2020-04-26 14:34:33 +0200

    move code to core/elastic

--- core/elastic.py
+++ core/elastic.py
@@ -2,8 +2,10 @@ import os
 import json
 import docker
 from tqdm import tqdm
-from elasticsearch import Elasticsearch, helpers
-from config import IMAGE_TAG, CONTAINER_NAME
+from elasticsearch import helpers
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import MultiMatch
+from config import IMAGE_TAG, CONTAINER_NAME, RUN_DIR, DOCS
 
 
 settings = {"settings":
@@ -15,11 +17,11 @@ settings = {"settings":
 
 def start_container():
     client = docker.from_env(timeout=86400)
-    container = client.containers.run(IMAGE_TAG,
-                                      ports={'9300/tcp': 9300, '9200/tcp': 9200},
-                                      name=CONTAINER_NAME,
-                                      environment={"discovery.type": "single-node"},
-                                      detach=True)
+    client.containers.run(IMAGE_TAG,
+                          ports={'9300/tcp': 9300, '9200/tcp': 9200},
+                          name=CONTAINER_NAME,
+                          environment={"discovery.type": "single-node"},
+                          detach=True)
     print("Elasticsearch is running.")
 
 
@@ -117,3 +119,67 @@ def index(es, DOCS, BULK=True, IDX_NAME=None):
         single_idx(es, DOCS, BULK, IDX_NAME)
     else:
         multi_idx(es, DOCS, BULK)
+
+
+def query_multi_idx(es, queries, DOCS):
+    for idx, path in DOCS.items():
+        with open('./runs/' + idx, 'w') as run:
+            for num, title in queries.items():
+                query = MultiMatch(query=title,
+                                   fields=['abstract', 'body_text'],
+                                   fuzziness='AUTO')
+                s = Search(using=es, index=idx).query(query)
+                s.execute()
+                count = 1
+                for hit in s[:1000]:
+                    line = num + ' Q0 ' + hit.paper_id + ' ' + str(count) + ' ' + str(hit.meta.score) + ' irc_base\n'
+                    run.write(line)
+                    count += 1
+
+
+def query_single_idx(es, meta, valid, queries, IDX_NAME, write_run: bool):
+
+    base_rank = {}
+
+    for num, title in queries.items():
+        query = MultiMatch(query=title,
+                           fields=['abstract', 'body_text'],
+                           fuzziness='AUTO')
+        s = Search(using=es, index=IDX_NAME).query(query)
+        s.execute()
+        count = 1
+        id_store = []
+        for hit in s[:5000]:
+            cord_uid_series = meta[meta['sha'] == hit.sha]['cord_uid']
+            cord_uid = None
+            try:
+                if len(cord_uid_series.values) > 1:
+                    pass
+                cord_uid = cord_uid_series.values[0]
+            except:
+                continue
+
+            if cord_uid in valid.values and cord_uid not in id_store:
+                base_rank[num] = {count: {'cord_uid': cord_uid, 'score': hit.meta.score}}
+                id_store.append(cord_uid)
+                count += 1
+
+                if count > 1000:
+                    break
+
+    if write_run:
+        with open(RUN_DIR + IDX_NAME, 'w') as run:
+            for top in range(1,31):
+                for rank in range(1,1001):
+                    base_rank[top][rank]['cord_uid']
+                    line = str(top) + ' Q0 ' + base_rank[top][rank]['cord_uid'] + ' ' + str(rank) + ' ' + base_rank[top][rank]['score'] + ' irc_base\n'
+                    run.write(line)
+
+    return base_rank
+
+
+def query(es, meta, valid, queries, write_run=True, IDX_NAME=None):
+    if IDX_NAME:
+        query_single_idx(es, meta, valid, queries, IDX_NAME, write_run)
+    else:
+        query_multi_idx(es, queries, DOCS=DOCS)
