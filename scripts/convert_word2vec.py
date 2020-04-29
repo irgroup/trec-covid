17600dae1df58a777c090e0ef8544a7c17470d5f
breuert <timo.breuer@th-koeln.de>
2020-04-29 16:56:33 +0200

    change name of downloaded file in convert_word2vec.py

--- scripts/convert_word2vec.py
+++ scripts/convert_word2vec.py
@@ -5,6 +5,6 @@ from config.config import EMBED_DIR
 
 
 if __name__ == '__main__':
-    model = gensim.models.KeyedVectors.load_word2vec_format('./download/bio_embedding_intrinsic', binary=True)
+    model = gensim.models.KeyedVectors.load_word2vec_format('./download/12551759', binary=True)
     mkdir(EMBED_DIR)
     model.save_word2vec_format(os.path.join(EMBED_DIR, 'bio_embedding_intrinsic.txt'), binary=False)
