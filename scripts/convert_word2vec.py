436a04ac6c24ed92bd733353135593e9e628e7b4
breuert <timo.breuer@th-koeln.de>
2020-05-04 11:31:35 +0200

    add support for spacy

--- scripts/convert_word2vec.py
+++ scripts/convert_word2vec.py
@@ -8,3 +8,5 @@ if __name__ == '__main__':
     model = gensim.models.KeyedVectors.load_word2vec_format('./download/12551759', binary=True)
     mkdir(EMBED_DIR)
     model.save_word2vec_format(os.path.join(EMBED_DIR, 'bio_embedding_intrinsic.txt'), binary=False)
+    os.system("gzip -k bio_embedding_intrinsic.txt")
+    os.system("python -m spacy init-model en ./embedding/spacy.bioemb.model --vectors-loc ./embedding/bio_embedding_intrinsic.txt.gz")
